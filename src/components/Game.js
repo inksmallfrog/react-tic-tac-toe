a3a0bca2f03a371d318438a4a3c670b37baa4952
inksmallfrog <inksmallfrog@gmail.com>
2017-04-24 08:58:06 +0800

    add some comments and props check

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,12 +2,14 @@
 * @Author: inksmallfrog
 * @Date:   2017-04-24 07:13:12
 * @Last Modified by:   inksmallfrog
-* @Last Modified time: 2017-04-24 08:46:23
+* @Last Modified time: 2017-04-24 08:56:38
 */
 
 'use strict';
 require ('styles/game.scss')
+
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Grid extends React.Component{
     constructor(props){
@@ -26,6 +28,19 @@ class Grid extends React.Component{
         )
     }
 }
+Grid.defaultProps = {
+    chess: {
+        id: 0,
+        state: ''
+    }
+}
+Grid.propTypes = {
+    playChess: PropTypes.func.isRequired,
+    chess: PropTypes.shape({
+        id: PropTypes.number,
+        state: PropTypes.oneOf(['', 'X', 'O'])
+    })
+}
 
 class Board extends React.Component{
     constructor(props){
@@ -40,6 +55,13 @@ class Board extends React.Component{
             </div>
         )
     }
+}
+Board.propTypes = {
+    playChess: PropTypes.func.isRequired,
+    chesses: PropTypes.arrayOf(PropTypes.shape({
+                                id: PropTypes.number,
+                                state: PropTypes.oneOf(['', 'X', 'O'])
+                               })).isRequired
 }
 
 class StatePanel extends React.Component{
@@ -70,7 +92,11 @@ class Game extends React.Component{
             gameState: 'X'
         }
     }
-
+    /*
+     * 检查游戏是否出现胜利者
+     * @return<boolean>: 是否出现胜利者
+     * 副作用：若出现胜利者，修改state.gameState
+     */
     checkWinner(){
         const chesses = this.state.chesses;
         const winStates = [
@@ -84,6 +110,7 @@ class Game extends React.Component{
             [6, 7, 8]
         ];
         let winner;
+        //是否存在满足胜利条件的格子
         winStates.some((winState) => {
             if(chesses[winState[0]].state != ''
                 && chesses[winState[0]].state == chesses[winState[1]].state
@@ -102,6 +129,11 @@ class Game extends React.Component{
         return false;
     }
 
+    /*
+     * 落子事件
+     * @params: index, 点击格子的index
+     * @return <Function>: 处理对应格子的事件
+     */
     play(index){
         return ()=>{
             let state = this.state.gameState;
