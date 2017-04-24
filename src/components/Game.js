04e63883c175943a111394879fa1f3af0b9ca151
inksmallfrog <inksmallfrog@gmail.com>
2017-04-24 08:47:05 +0800

    local play

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,7 +2,7 @@
 * @Author: inksmallfrog
 * @Date:   2017-04-24 07:13:12
 * @Last Modified by:   inksmallfrog
-* @Last Modified time: 2017-04-24 07:51:01
+* @Last Modified time: 2017-04-24 08:46:23
 */
 
 'use strict';
@@ -12,10 +12,17 @@ import React from 'react';
 class Grid extends React.Component{
     constructor(props){
         super(props);
+        this.handleClick = (e) => {
+            if(!this.props.chess.state){
+                this.props.playChess();
+            }
+            e.stopPropagation();
+            e.preventDefault();
+        }
     }
     render(){
         return(
-            <span className="grid" >{this.props.value}</span>
+            <div className="grid" onClick={this.handleClick}>{this.props.chess.state}</div>
         )
     }
 }
@@ -27,9 +34,8 @@ class Board extends React.Component{
     render(){
         return(
             <div className="grids-panel">
-                {this.props.chess.map((value, index)=>{
-                    console.log(value);
-                    return <Grid key={index} state={value}/>
+                {this.props.chesses.map((value, index)=>{
+                    return <Grid key={index} chess={value} playChess={this.props.play(index)}/>
                 })}
             </div>
         )
@@ -37,10 +43,13 @@ class Board extends React.Component{
 }
 
 class StatePanel extends React.Component{
+    constructor(props){
+        super(props);
+    }
     render(){
         return(
             <div>
-                <h4>Some State</h4>
+                <h4>{this.props.gameState}</h4>
                 <ul></ul>
             </div>
         )
@@ -50,16 +59,69 @@ class StatePanel extends React.Component{
 class Game extends React.Component{
     constructor(props){
         super(props);
-        //The size of Gameboard is 3 * 3
+        let chesses = Array.apply(null, new Array(9)).map((value, index) => {
+           return {
+                id: index,
+                state: ''
+           }
+        });
         this.state = {
-            chess: [].fill.call(new Array(9), '')
+            chesses: chesses,
+            gameState: 'X'
         }
     }
+
+    checkWinner(){
+        const chesses = this.state.chesses;
+        const winStates = [
+            [0, 1, 2],
+            [0, 3, 6],
+            [0, 4, 8],
+            [1, 4, 7],
+            [2, 5, 8],
+            [2, 4, 6],
+            [3, 4, 5],
+            [6, 7, 8]
+        ];
+        let winner;
+        winStates.some((winState) => {
+            if(chesses[winState[0]].state != ''
+                && chesses[winState[0]].state == chesses[winState[1]].state
+                && chesses[winState[0]].state == chesses[winState[2]].state){
+                winner = chesses[winState[0]].state;
+                return true;
+            }
+            return false;
+        });
+        if(winner){
+            this.setState({
+                gameState: 'Winner is ' + winner
+            })
+            return true;
+        }
+        return false;
+    }
+
+    play(index){
+        return ()=>{
+            let state = this.state.gameState;
+            if(state == 'X' || state == 'O'){
+                let chesses = this.state.chesses.slice();
+                chesses[index].state = state;
+                this.setState({
+                    chesses: chesses,
+                    gameState: state == 'X' ? 'O' : 'X'
+                })
+                this.checkWinner();
+            }
+        }
+    }
+
     render(){
         return(
             <section>
-                <Board chess={this.state.chess}/>
-                <StatePanel />
+                <Board chesses={this.state.chesses} play={this.play.bind(this)}/>
+                <StatePanel gameState={this.state.gameState}/>
             </section>
         )
     }
